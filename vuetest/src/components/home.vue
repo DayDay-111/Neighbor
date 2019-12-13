1e94610154711e752553c34e9f77587e16d2fa80
DayDay-111 <q416702285@gmail.com>
2019-12-12 22:34:54 -0500

    1

--- vuetest/src/components/home.vue
+++ vuetest/src/components/home.vue
@@ -15,20 +15,25 @@
           <el-menu-item index="2-3">create subject</el-menu-item>
       </el-submenu>
       <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
+        <template slot="title"><i class="el-icon-setting"></i>friend</template>
+          <el-menu-item index="3-1">FriendList</el-menu-item>
+          <el-menu-item index="3-2">Friend subject</el-menu-item>
+          <el-menu-item index="3-3">create subject</el-menu-item>
       </el-submenu>
+      <el-submenu index="4">
+        <template slot="title"><i class="el-icon-setting"></i>neighbor</template>
+          <el-menu-item index="4-1">neighbor</el-menu-item>
+          <el-menu-item index="4-2">neighbor subject</el-menu-item>
+          <el-menu-item index="4-3">create subject</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="5" >
+        <i class="el-icon-document"></i>
+        <span slot="title">search</span>
+      </el-menu-item>
+      <el-menu-item index="6" >
+        <i class="el-icon-document"></i>
+        <span slot="title">exit</span>
+      </el-menu-item>
     </el-menu>
   </el-aside>
   
